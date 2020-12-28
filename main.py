7c62015057578eaf79f8f53807fede65fd45290d
Marek <uhliar.marek@gmail.com>
2020-12-28 01:21:47 +0100

    little modification

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import asyncio
-from kasa import Discover
+
 from kasa import SmartPlug
+
 
 def turn_on_button():
     plug = SmartPlug("192.168.0.189")
@@ -18,13 +19,11 @@ def turn_off_button():
 root = Tk()
 root.geometry('300x200')
 root.title("Smart Plug")
-window_photo = PhotoImage(file = 'icon.png')
+window_photo = PhotoImage(file='icon.png')
 root.iconphoto(False, window_photo)
-
 
 my_label = Label(root, text="Ovládanie TP-link smart zásuvky HS110")
 my_label.pack(pady=10)
-
 
 my_button = Button(root, text="ON", command=turn_on_button, width=10)
 my_button.pack(pady=10)
