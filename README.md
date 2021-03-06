25f0670aaefe03d56ffa8424d4d92a6b1454227d
Marek <uhliar.marek@gmail.com>
2021-03-06 15:04:47 +0100

    Add config file

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Smart_plug_GUI
 GUI for TP-link smart plug 
 
-The basic control software was developed in Python3. Before a run it, install necessary packages:
+The basic control software was developed in Python3.8 Before a run it, install the necessary packages:
 
     
-    pip install pySerial
     pip install python-kasa
     pip install asyncio
-    pip install beebotte
+    
--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 from tkinter import *
 import asyncio
-
 from kasa import SmartPlug
+from configparser import ConfigParser
+
+# Read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")
+
+device = config_object["DEVICECONFIG"]
+debug = config_object["DEBUG"]
 
 
 def turn_on_button():
-    plug = SmartPlug("192.168.0.189")
-    asyncio.run(plug.update())
-    asyncio.run(plug.turn_on())
+    if (debug["DEBUG"]) == "TRUE":
+        print("DEVICE ON")
+    else:
+        plug = SmartPlug(device["host"])
+        print(device["host"])
+        # asyncio.run(plug.update())
+        # asyncio.run(plug.turn_on())
 
 
 def turn_off_button():
-    plug = SmartPlug("192.168.0.189")
-    asyncio.run(plug.update())
-    asyncio.run(plug.turn_off())
+    if (debug["DEBUG"]) == "TRUE":
+        print("DEVICE OFF")
+    else:
+        plug = SmartPlug(device["host"])
+        # asyncio.run(plug.update())
+        # asyncio.run(plug.turn_off())
 
 
 root = Tk()
 root.geometry('300x200')
 root.title("Smart Plug")
-window_photo = PhotoImage(file='icon.png')
+window_photo = PhotoImage(file='icons/icon.png')
 root.iconphoto(False, window_photo)
 
 my_label = Label(root, text="Ovládanie TP-link smart zásuvky HS110")
