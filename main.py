ade6f765a3c02b451cbb847165669a8f197eed29
Marek <uhliar.marek@gmail.com>
2021-03-28 22:09:21 +0200

    new version of GUI with better graphic of buttons

--- main.py
+++ main.py
@@ -10,38 +10,78 @@ config_object.read("config.ini")
 device = config_object["DEVICECONFIG"]
 debug = config_object["DEBUG"]
 
-
-def turn_on_button():
-    if (debug["DEBUG"]) == "TRUE":
-        print("DEVICE ON")
-    else:
-        plug = SmartPlug(device["host"])
-        asyncio.run(plug.update())
-        asyncio.run(plug.turn_on())
+State1 = 0
+State2 = 0
 
 
-def turn_off_button():
-    if (debug["DEBUG"]) == "TRUE":
-        print("DEVICE OFF")
-    else:
+def button1(event):
+    global State1
+
+    if State1 == 1:
+        winbtn1.config(image=btnoff)
         plug = SmartPlug(device["host"])
         asyncio.run(plug.update())
         asyncio.run(plug.turn_off())
+        State1 = 0
+    else:
+        winbtn1.config(image=btnon)
+        plug = SmartPlug(device["host"])
+        asyncio.run(plug.update())
+        asyncio.run(plug.turn_on())
+        State1 = 1
 
 
-root = Tk()
-root.geometry('300x200')
-root.title("Smart Plug")
-window_photo = PhotoImage(file='icons/icon.png')
-root.iconphoto(False, window_photo)
+def button2(event):
+    global State2
+    if State2 == 1:
+        winbtn2.config(image=btnoff)
+        print("tralala off")
+        State2 = 0
+    else:
+        winbtn2.config(image=btnon)
+        print("tralala on")
+        State2 = 1
 
-my_label = Label(root, text="Ovládanie TP-link smart zásuvky HS110")
-my_label.pack(pady=10)
 
-my_button = Button(root, text="ON", command=turn_on_button, width=10)
-my_button.pack(pady=10)
+# GUI
 
-my_button2 = Button(root, text="OFF", command=turn_off_button, width=10)
-my_button2.pack(pady=10)
+app = Tk()
+app.title("SmartPlug GUI")
+app.geometry("250x300")
+app.resizable(0, 0)
 
-root.mainloop()
+relay1_label = Label(app, font="Consolas 12", text="SmartPlug")
+relay2_label = Label(app, font="Consolas 12", text="Tralala")
+
+btnoff = PhotoImage(file="icons/off.png")
+btnon = PhotoImage(file="icons/on.png")
+
+winbtn1 = Label(app, image=btnoff)
+winbtn1.bind("<Button-1>", button1)
+winbtn1.pack()
+
+winbtn2 = Label(app, image=btnoff)
+winbtn2.bind("<Button-1>", button2)
+winbtn2.pack()
+
+relay1_label.pack()
+relay2_label.pack()
+
+winbtn1.place(x=150, y=100)
+winbtn2.place(x=150, y=150)
+
+relay1_label.place(x=50, y=105)
+relay2_label.place(x=50, y=155)
+
+# Correct startup
+if State1 == 0:
+    winbtn1.config(image=btnoff)
+else:
+    winbtn1.config(image=btnon)
+
+if State2 == 0:
+    winbtn2.config(image=btnoff)
+else:
+    winbtn2.config(image=btnon)
+
+app.mainloop()
